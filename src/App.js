c82fd5f01836b8ea96ba1734dbda22142b8c8081
zh4ngyj <165157272+zh4ngyj@users.noreply.github.com>
2025-02-07 21:00:01 +0800

    Add files via upload
    initial

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+function Square({value, onSquareClick}) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+  function handleClick(){
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick}/>
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
+      </div>
+    </>
+
+  )
+}
